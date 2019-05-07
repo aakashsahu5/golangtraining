1b96220338c8f2639d1cba4273b06d8c139d1bb7
aakash.sahu <aakash.sahu@grab.com>
2019-05-07 14:59:47 +0530

    test

---
+++ test.go
@@ -0,0 +1,16 @@
+// test
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Contains(`Hello, 
+	playground sdfkj nsdjb kcnsdjk
+	vnjksdn dmv ksd vdvdsvn3984u32yrewbyr7t
+	748yt7443 n&^%$%*`, "&^%"))
+
+}
